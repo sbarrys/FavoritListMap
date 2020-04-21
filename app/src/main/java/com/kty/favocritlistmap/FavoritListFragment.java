5275ee9663099dd92c39567721ff2895057dcfae
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-04-21 15:37:24 +0900

    [2020/04/21]Fragment기본설정

---
+++ app/src/main/java/com/kty/favocritlistmap/FavoritListFragment.java
@@ -0,0 +1,25 @@
+package com.kty.favocritlistmap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FavoritListFragment extends Fragment {
+
+    public FavoritListFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorit_list, container, false);
+    }
+
+
+}
