b6eab7295700013bc63bc1e1e856e2a9634bc72e
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-04-21 16:25:23 +0900

    [2020/04/21]Fragment - SubFragment 설정

---
+++ app/src/main/java/com/kty/favocritlistmap/SubTwoFragment.java
@@ -0,0 +1,21 @@
+package com.kty.favocritlistmap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubTwoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sub_two, container, false);
+    }
+
+
+}
